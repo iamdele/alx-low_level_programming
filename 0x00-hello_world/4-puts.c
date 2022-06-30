8b652b1574f3398c54ecce5fd30e57b37ddc9af7
iamdele <emmanuelabraham7512@gmail.com>
2022-06-30 15:58:10 -0700

    git fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 puts("\"Programming is like buiding a multilingual puzzle");
 return (0);
 }
-
