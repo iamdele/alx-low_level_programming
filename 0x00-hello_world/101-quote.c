b3f6baefcc76a428f728b258e569addf1afe99c6
iamdele <emmanuelabraham7512@gmail.com>
2022-06-30 21:44:02 -0700

    hott fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,2 @@
-#include <stdio.h>
-#include <unistd.h>
-
-/**
- * main - prints exactly "and that place of art is useful"
- *  - Dora Korpar, 2015-10-19",
- *  followed by a new line, to the standard error.
- *  Return: Always 0 (success)
- */
-int main(void)
-{
-write(2. "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
-}
-
+#!/bin/bash
+gcc -masm=intel $CFILE -S > $(echo $CFILE|cut -d"." -f1).s
