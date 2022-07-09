196788282f070b1562b2d92cfa73957aa952f604
iamdele <emmanuelabraham7512@gmail.com>
2022-07-08 21:48:00 -0700

    more functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN.H
+#define MAIN_H
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -5,6 +7,8 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-void print_diagonal(int n)
+void print_diagonal(int n);
 void print_square(int size);
-void print_triangle(int size);
+void fizz_buzz(void);
+void print_square(int size);
+#endif
